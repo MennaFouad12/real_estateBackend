6dc14bc679528c509c8b562bff4fa6be2313959c
MennaFouad12 <mm6331329@gmail.com>
2025-11-01 21:32:00 +0200

    Update project code and fix issues

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ connectDB()
   app.use("/api/user",userRouter);
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`✅ Server running on http://localhost:${PORT}`);
+// });
+
+
+app.get('/', (req, res) => res.send('Hello from Vercel!'));
+
+module.exports = app;
