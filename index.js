3064d3348a82f747cce76e0080367a6dac40553a
MennaFouad12 <mm6331329@gmail.com>
2025-11-01 21:11:50 +0200

    Update project code and fix issues

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ connectDB()
 
   app.use("/api/user",userRouter);
 // Start the server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
